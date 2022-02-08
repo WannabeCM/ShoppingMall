b900c93040677a42a8a30ef3b988f6a8481a17b4
Jason Park_朴城范_박성범 <87704703+WannabeCM@users.noreply.github.com>
2022-02-08 22:09:05 +0900

    project init

---
+++ src/main/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import './index.css';
+import axios from "axios";
+
+function MainPage() {
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(
+        function () {
+            axios
+            .get('https://f2de9abb-923d-447b-b8ca-2167db83d737.mock.pstmn.io/products')
+            .then(function (result) {
+                const products = result.data.products;
+                setProducts(products);
+            }).catch(function (error) {
+                console.error('에러 발생 : ', error);
+        })
+        },[])
+        
+       
+    return ( 
+<div>
+    <div id="header">
+        <div id="header-area">
+            <img src="images/icons/logo.png" alt="bum's 마켓" />
+        </div>
+    </div>
+    <div id="body">
+        <div id="banner">
+            <img src="images/banners/banner1.png" alt="배너1" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+            {products.map(function(product, index){
+                    return (
+                    <div className="product-card">
+                        <div>
+                                <img className="product-img"
+                                    src={product.imageUrl} />
+                        </div>
+                       <div className='product-contents'>
+                           <span className='product-name'>
+                               {product.name}
+                            </span>
+                            <span className='product-price'>
+                                {product.price}원
+                            </span>
+                            <div className='product-seller'>
+                                <img className='product-avatar' src='images/icons/avatar.png' />
+                                <span>{product.seller}</span>
+                            </div>
+                        </div>
+                      </div>
+                    )
+                })
+            };
+          
+            <div className="product-card"></div>
+            <div className="product-card"></div>
+            <div className="product-card"></div>
+            <div className="product-card"></div>
+        </div>
+    </div>
+    <div id="footer"></div>
+</div>
+    );
+}
+
+export default MainPage;
